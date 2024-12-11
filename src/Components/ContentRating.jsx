ef917ac56b1c5df2ca3e9008b37d2ffcf5c05be9
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-12-11 18:58:55 -0500

    initial commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,16 +1,34 @@
-
-import React, { Component } from 'react';
-import './ContentRating.css';
+import React, { Component } from "react";
+import "./ContentRating.css";
 
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      handleLike:()=>{
+        this.setState((preState)=>({likes:preState.likes+1}))
+      },
+      handleDislike:()=>{
+        this.setState((preState)=>({dislikes:preState.dislikes+1}))
+      }
+    };
   }
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <>
+        <div className="content-rating">
+          <p>Testing Content</p>
+          <div className="rating-button">
+            <button onClick={this.state.handleLike} className="like-button">Like ({this.state.likes})</button>
+            <button onClick={this.state.handleDislike} className="dislike-button">
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+        </div>
+      </>
     );
   }
 }
