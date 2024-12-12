468d1e3d1ef8a8a05e922c0dd963b6220216a3b3
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-12-11 19:05:49 -0500

    calculate total ratings

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -7,12 +7,19 @@ class ContentRating extends Component {
     this.state = {
       likes: 0,
       dislikes: 0,
-      handleLike:()=>{
-        this.setState((preState)=>({likes:preState.likes+1}))
+      handleLike: () => {
+        this.setState((preState) => ({
+          likes: preState.likes + 1,
+          totalRatings: preState.totalRatings + 1,
+        }));
       },
-      handleDislike:()=>{
-        this.setState((preState)=>({dislikes:preState.dislikes+1}))
-      }
+      handleDislike: () => {
+        this.setState((preState) => ({
+          dislikes: preState.dislikes + 1,
+          totalRatings: preState.totalRatings + 1,
+        }));
+      },
+      totalRatings: 0,
     };
   }
 
@@ -22,11 +29,17 @@ class ContentRating extends Component {
         <div className="content-rating">
           <p>Testing Content</p>
           <div className="rating-button">
-            <button onClick={this.state.handleLike} className="like-button">Like ({this.state.likes})</button>
-            <button onClick={this.state.handleDislike} className="dislike-button">
+            <button onClick={this.state.handleLike} className="like-button">
+              Like ({this.state.likes})
+            </button>
+            <button
+              onClick={this.state.handleDislike}
+              className="dislike-button"
+            >
               Dislike ({this.state.dislikes})
             </button>
           </div>
+          <p>Total Ratings: {this.state.totalRatings}</p>
         </div>
       </>
     );
